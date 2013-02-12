f14b3409107f3a5e737c8ee061be46a3a3548619
xseignard <xavier.seignard@gmail.com>
2013-02-12 13:58:29 +0100

    some cleaning

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,43 +3,32 @@ require.config({
 	baseUrl: 'scripts/modules',
 	// vendor lib paths
 	paths: {
-		box2d: '../vendor/Box2d.min/index',
-		requestAnimationFrame: '../vendor/raf/raf',
 		jquery: '../vendor/jquery/jquery',
 		io: '../vendor/socket.io/dist/socket.io',
-		stats: '../vendor/stats/index',
 		webpd: '../vendor/webpd/index'
-	},
-	shim: {
-		// box2d web shim
-		'box2d': {
-			exports: 'Box2D'
-		},
-		'stats': {
-			exports: 'Stats'
-		}
 	}
 });
 
 // start the main app logic.
-require(['jquery', 'io', 'scene'],
-function($, io, Scene) {
+require(['jquery', 'io'],
+function($, io) {
 	$(function() {
-		console.log(Pd);
 		var patch,sketch, docH,docW;
 		docH = $(window).height();
 		docW = $(window).width();
-		$.get('test.pd', function(patchFile) {
+
+		// load pd patch
+		$.get('patch/test.pd', function(patchFile) {
 			patch = Pd.compat.parse(patchFile);
             console.log('WebPd patch ready');
 		    patch.play();
 		});
 		
 		// connect the browser to the server through websocket
-		var socket = io.connect('http://localhost:8080');
+		var socket = io.connect(window.location.href);
 		// when the 'tweet' message is recieved
 		socket.on('tweet', function (data) {
-			console.log(data);
+			// send the sound name to the pd patch
 			patch.send(data.sound,1);
 		});
 	});
